58dac53af47c185a47de4e58048a62366f865c80
Siméon DAOUDA <simeondaouda@gmail.com>
2024-11-11 15:20:06 +0100

    blog & activity log

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,35 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Spatie\Activitylog\LogOptions;
+use Firefly\FilamentBlog\Traits\HasBlog;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    use HasBlog;
+
+    public function canComment(): bool
+    {
+        // your conditional logic here
+        return true;
+    }
+
+    use LogsActivity;
+
+    // protected $fillable = ['name', 'text'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logOnly(['name', 'text']);
+    }
 
     /**
      * The attributes that are mass assignable.
